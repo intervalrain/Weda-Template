a655b3cc4fe29c2ee246273673b1bbea5f14802d
rainhu <intervalrain@gmail.com>
2026-02-09 15:55:48 +0800

    chore: remove obsolete design docs and NatsConnectionConfig

--- src/Weda.Core/Infrastructure/Messaging/NatsConnectionConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Weda.Core.Infrastructure.Messaging;
-
-public class NatsConnectionConfig
-{
-    public required string Name { get; set; }
-    public required string Url { get; set; }
-    public string? CredFile { get; set; }
-}
